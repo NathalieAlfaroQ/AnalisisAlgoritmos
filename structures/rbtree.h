19a47c41ef7802d04915ce78b2c9837d2400f29d
Nathalie <nathalie.alfaro@ucr.ac.cr>
2024-06-19 18:59:45 -0600

    rn

--- structures/rbtree.h
+++ structures/rbtree.h
@@ -8,7 +8,11 @@
 // Bibliotecas
 #include <iostream>
 
-enum colors {RED, BLACK};
+enum colors
+{
+    RED,
+    BLACK
+};
 
 // Nodos del arbol:
 template <typename T>
@@ -19,153 +23,288 @@ public:
     T key;
     rbtnode<T> *p, *left, *right;
     enum colors color;
-    
+
     // Constructor por omision.
-    rbtnode() {
-    };
-    
-    //Inicializacion de datos miembro.
-    rbtnode (const T& k, rbtnode<T> *w = nullptr, rbtnode<T> *y = nullptr, rbtnode<T> *z = nullptr, enum colors c = RED): key(k), p(w), left(y), right(z), color(c) {};
-    
+    rbtnode() : p(nullptr), left(nullptr), right(nullptr), color(RED) {}
+
+    // Inicializacion de datos miembro.
+    rbtnode(const T &k, rbtnode<T> *w = nullptr, rbtnode<T> *y = nullptr, rbtnode<T> *z = nullptr, enum colors c = RED) : key(k), p(w), left(y), right(z), color(c){};
+
     // Destructor
-    ~rbtnode() {
-    };
+    ~rbtnode(){};
 }; // End class rbtnode
 
 // Arbol rojinegro:
 template <typename T>
-class rbtree {
+class rbtree
+{
 public:
-    rbtnode<T> *root;    // raiz del arbol
-    rbtnode<T> *nil;     // nodo nil (hoja) del arbol
-    
-    rbtree() {
+    rbtnode<T> *root; // raiz del arbol
+    rbtnode<T> *nil;  // nodo nil (hoja) del arbol
+
+    rbtree()
+    {
         // Constructor (crea un arbol vacio)
-    };
-    
-    ~rbtree() {
-        // Destructor (borra el arbol)
+        nil = new rbtnode<T>();
+        nil->color = BLACK;
+        root = nil;
     };
 
-    leftRotate (T, x) {
-        y = x->right;
+    ~rbtree()
+    {
+        // Destructor (borra el arbol)
+        destroy_tree(root);
+        delete nil;
+    };
+
+    void destroy_tree(rbtnode<T> *x)
+    {
+        if (x != nil)
+        {
+            destroy_tree(x->left);
+            destroy_tree(x->right);
+            delete x;
+        } // End if
+    } // End destroy_tree
+
+    void rightRotate(rbtnode<T> *x)
+    {
+        rbtnode<T> *y = x->left;
+        x->left = y->right;
+
+        if (y->right != nullptr)
+        {
+            y->right->p = x;
+        } // End if
+
+        y->p = x->p;
+
+        if (x->p == nullptr)
+        {
+            root = y;
+        }
+        else if (x == x->p->right)
+        {
+            x->p->left = y;
+        }
+        else
+        {
+            x->p->left = y;
+        } // End if
+
+        y->right = x;
+        x->p = y;
+    } // End rightRotate
+
+    void leftRotate(rbtnode<T> *x)
+    {
+        rbtnode<T> *y = x->right;
         x->right = y->left;
 
-        if (y->left != T->nil) {
+        if (y->left != this->nil)
+        {
             y->left->p = x;
         } // End if
 
         y->p = x->p;
 
-        if (x->p == T->nil) {
-            T->root = y;
-        } else if (x == x->p->left) {
+        if (x->p == this->nil)
+        {
+            this->root = y;
+        }
+        else if (x == x->p->left)
+        {
             x->p->left = y;
-        } else {
+        }
+        else
+        {
             x->p->right = y;
         } // End if
 
         y->left = x;
         x->p = y;
     } // End leftRotate
-    
-    insertFixUp(T, z) {
-        while (z->p->color == RED) {
 
-            if (z->p == z->p->p->left) {
-                y = z->p->p->right;
+    void insertFixUp(rbtnode<T> *z)
+    {
+        while (z->p->color == RED)
+        {
 
-                if (y->color == RED) {
+            if (z->p == z->p->p->left)
+            {
+                rbtnode<T> *y = z->p->p->right;
+
+                if (y->color == RED)
+                {
                     z->p->color = BLACK;
                     y->color = BLACK;
                     z->p->p->color = RED;
                     z = z->p->p;
-                } else {
-                    if (z == z->p->right) {
+                }
+                else
+                {
+                    if (z == z->p->right)
+                    {
                         z = z->p;
-                        leftRotate(T, z);
+                        leftRotate(z);
                     } // End if
                     z->p->color = BLACK;
                     z->p->p->color = RED;
-                    rightRotate(T, z->p->p);
+                    rightRotate(z->p->p);
                 } // End if
-            } else { 
-                y = z->p->p->left;
+            }
+            else
+            {
+                rbtnode<T> *y = z->p->p->left;
 
-                if (y->color == RED) {
+                if (y->color == RED)
+                {
                     z->p->color = BLACK;
                     y->color = BLACK;
                     z->p->p->color = RED;
                     z = z->p->p;
-                } else {
-                    if (z == z->p->left) {
+                }
+                else
+                {
+                    if (z == z->p->left)
+                    {
                         z = z->p;
-                        rightRotate(T, z);
+                        rightRotate(z);
                     } // End if
                     z->p->color = BLACK;
                     z->p->p->color = RED;
-                    leftRotate(T, z->p->p);
+                    leftRotate(z->p->p);
                 } // End if
             } // End if
         } // End while
-        T->root->color = BLACK;
+        this->root->color = BLACK;
     } // End insertFixUp
 
-    void Insert(rbtnode<T>* z) {
+    void Insert(rbtnode<T> *z)
+    {
         // Inserta el nodo z en la posicion que le corresponde en el arbol.
-        x = T->root;
-        y = T->nil;
+        rbtnode<T> *x = this->root;
+        rbtnode<T> *y = this->nil;
 
-        while (x != T->nil) {
+        while (x != this->nil)
+        {
             y = x;
 
-            if (z->key < x->key) {
+            if (z->key < x->key)
+            {
                 x = x->left;
-            } else {
+            }
+            else
+            {
                 x = x->right;
             } // End if
         } // End while
 
         z->p = y;
 
-        if (y == T->nil) {
-            T->root = z;
-        } else if (z->key < y->key) {
+        if (y == this->nil)
+        {
+            this->root = z;
+        }
+        else if (z->key < y->key)
+        {
             y->left = z;
-        } else {
+        }
+        else
+        {
             y->right = z;
         } // End if
 
-        z->left = T->nil;
-        z->right = T->nil;
+        z->left = this->nil;
+        z->right = this->nil;
         z->color = RED;
 
-        insertFixUp(T, z);
-    };
-    
-    void InorderWalk(rbtnode<T> *x) {
+        insertFixUp(z);
+    }; // End Insert
+
+    void InorderWalk(rbtnode<T> *x)
+    {
         // Recorre en orden el subarbol con raiz x, imprimiendo la llave de cada nodo en en una nueva linea de la salida estandar despues de recorrido el subarbol izquierdo y antes de recorrer el subarbol derecho.
-    };
-    
-    rbtnode<T>* Search(rbtnode<T> *x, const T& k) {
+        if (x != nil)
+        {
+            InorderWalk(x->left);
+            std::cout << x->key << std::endl;
+            InorderWalk(x->right);
+        } // End if
+    }; // End InorderWalk
+
+    rbtnode<T> *Search(rbtnode<T> *x, const T &k)
+    {
         // Busca la llave k iterativamente en el subarbol con raiz x. Si la encuentra devuelve un apuntador al nodo que la contiene, sino devuelve el nodo nil.
-    };
-    
-    rbtnode<T>* IterativeSearch(rbtnode<T> *x, const T& k) {
+        if (x == nil || k == x->key)
+        {
+            return x;
+        } // End if
+
+        if (k < x->key)
+        {
+            return Search(x->left, k);
+        }
+        else
+        {
+            return Search(x->right, k);
+        } // End if
+    }; // End Search
+
+    rbtnode<T> *IterativeSearch(rbtnode<T> *x, const T &k)
+    {
         // Busca la llave k iterativamente en el subarbol con raiz x. Si la encuentra devuelve un apuntador al nodo que la contiene, sino devuelve el nodo nil.
-    };
-    
-    rbtnode<T>* Minimum(rbtnode<T> *x) {
+        while (x != nil && k != x->key)
+        {
+            if (k < x->key)
+            {
+                x = x->left;
+            }
+            else
+            {
+                x = x->right;
+            } // End if
+        } // End while
+        return x;
+    }; // End IterativeSearch
+
+    rbtnode<T> *Minimum(rbtnode<T> *x)
+    {
         // Devuelve el nodo que tiene la llave menor. Si el arbol esta vacio, devuelve el nodo nil.
-    };
-    
-    rbtnode<T>* Maximum(rbtnode<T> *x) {
+        while (x->left != nil)
+        {
+            x = x->left;
+        } // End while
+        return x;
+    }; // End Minimum
+
+    rbtnode<T> *Maximum(rbtnode<T> *x)
+    {
         // Devuelve el nodo que tiene la llave mayor. Si el arbol esta vacio devuelve el nodo nil.
-    };
-    
-    rbtnode<T>* Successor(rbtnode<T> *x) {
+        while (x->right != nil)
+        {
+            x = x->right;
+        } // End while
+        return x;
+    }; // End Maximum
+
+    rbtnode<T> *Successor(rbtnode<T> *x)
+    {
         // Devuelve el nodo cuya llave es la que le sigue a la del nodo x. Si no existe el nodo, devuelve el nodo nil.
-    };
+        if (x->right != nil)
+        {
+            return Minimum(x->right);
+        } // End if
+
+        rbtnode<T> *y = x->p;
+
+        while (y != nil && x == y->right)
+        {
+            x = y;
+            y = y->p;
+        } // End while
+        return y;
+    }; // End Successor
 }; // End class rbtree
 
 #endif /* rbtree_h */
--- structures/readme.md
+++ structures/readme.md
@@ -15,25 +15,9 @@ Para cada estructura se hacen 2, una para insertar nodos aleatorios y otra para 
 
 - Lista doblemente enlazada con nodo centinela
 - Arbol de busqueda binaria
+- Arbol roji negro
 
 
 ### Creditos
 
 Nathalie Alfaro Quesada
-
-
-
-
-
-
-
-Implemente la clase árbol rojinegro usando la plantilla rbtree.h. Esta planti-
-lla incluye los mismos métodos que la clase árbol de búsqueda binario, excepto
-
-la operación de borrado: construcción [2 pts.], destrucción [3 pts.], inserción
-
-[8 pts.], búsqueda de llave recursiva [2 pts.] e iterativa [3 pts.], búsqueda del má-
-ximo [1 pto.], mínimo [1 pto.], sucesor de un nodo [2 pto.] y recorrido de un árbol
-
-en orden [3 pts.].
-
